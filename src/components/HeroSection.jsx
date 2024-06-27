b994883c5401fd68c609d2d7eceab34382f7ec39
Satyam Singh <satyamrajput7239@gmail.com>
2024-06-27 19:04:01 +0530

    feat: Implement Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import './HeroSection.css';
+
+const HeroSection = () => {
+    return (
+        <div className='hero-section'>
+            <img src='hero-image.png' alt='Hero Image for QTify' />
+        </div>
+    );
+};
+
+export { HeroSection };
